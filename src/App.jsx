aca06d601c18c688fe20e7e659b40e1f47cad715
crearwebdesign <oscar10034@gmail.com>
2024-09-16 18:28:13 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default App
