b3aff7f30f7818889ea96c229e01edd246a66718
crearwebdesign <oscar10034@gmail.com>
2024-10-27 15:20:21 -0400

    Act271024

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import {StoreContext} from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({category}) => {
 
@@ -9,6 +10,11 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
       <h2>Mejores platos cerca de Ti</h2>
+      <div className="food-display-list">
+        {food_list.map((item,index)=>{
+            return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image}/>
+        })}
+      </div>
     </div>
   )
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import './Cart.css'
 const Cart = () => {
   return (
     <div>
-      
+      <h1>Carrito</h1>
     </div>
   )
 }
