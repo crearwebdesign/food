676f5bf13a2050cfa71042624739a2cbb9269dc6
crearwebdesign <oscar10034@gmail.com>
2024-11-11 11:31:19 -0400

    Act111124

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
@@ -10,7 +10,13 @@ const FoodItem = ({ id, name, price, description, image }) => {
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
         {
-            
+            !itemCount
+            ? <img  className="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+            : <div className="food-item-counter">
+              <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+              <p>{itemCount}</p>
+              <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+            </div>
         }
       </div>
       <div className="food-item-info">
