fbf7dbbbc471b80d417c8a0abcbefa95806e0e56
crearwebdesign <oscar10034@gmail.com>
2024-12-09 22:49:34 -0400

    Act091224

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import NavBar from './components/NavBar/NavBar'
-
-
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>El Mejor Restaurant de la Ciudad Tomato</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>Contactanos</li>
+                <li>Delivery</li>
+                <li>Políticas de Privacidad</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>PONTE EN CONTACTO</h2>
+                <ul>
+                    <li>+584124606520</li>
+                    <li>oscar10034@gmail.comj</li>
+                </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 SoftMillenium 
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
